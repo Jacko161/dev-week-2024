ceea5e86fc3f1337aae198960d412eb5dd136079
Jackson Fleming <7390253+Jacko161@users.noreply.github.com>
2025-03-17 11:44:28 +1100

    Initial commit

---
+++ src/main/java/jfleming/devweek2024/DevWeek2024Application.java
@@ -0,0 +1,13 @@
+package jfleming.devweek2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevWeek2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevWeek2024Application.class, args);
+    }
+
+}
